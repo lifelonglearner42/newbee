f5f779a893fe930c4d29b9b57c0594c540e7d911
lifelonglearner42 <kutman.uu@gmail.com>
2020-01-08 08:35:54 -0600

    changed testingpushpull

--- Testingpushpull.py
+++ Testingpushpull.py
@@ -1,6 +1,10 @@
-def Hello():
-    print "Hello world"
+def Hello(msg):
+    print msg
 
+
+Hello("Hello people")
 //this is first comment
 
 //this is second comment after setting upstream
+
+//modified module and added comment after git-ignore .idea
